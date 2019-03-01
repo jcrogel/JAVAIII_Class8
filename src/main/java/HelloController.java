f0d2dae2e30903f33f2785f7eba5bc1f2107ec0b
Juan C Moreno <jcrogel@gmail.com>
2019-02-28 23:46:44 -0800

    Conditional rendering on JSF

--- src/main/java/HelloController.java
+++ src/main/java/HelloController.java
@@ -1,8 +1,18 @@
 import java.io.Serializable;
 
 public class HelloController implements Serializable {
+    public String msg="";
 
     public String sayHello() {
-        return "Hello World via JSF";
+        if (msg.isEmpty()){
+            return "Hello World via JSF";
+        }
+        return "Hello " +  msg + " via JSF";
+    }
+
+
+    public void setMsg(String value){
+        this.msg = value;
     }
 }
+
--- src/main/java/com/jcrogel/jax/JAXHelloWorld.java
+++ src/main/java/com/jcrogel/jax/JAXHelloWorld.java
@@ -3,12 +3,8 @@ package com.jcrogel.jax;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
 
 @Path("/hello")
 public class JAXHelloWorld {
@@ -17,10 +13,7 @@ public class JAXHelloWorld {
     @Path("/")
     @Produces({MediaType.APPLICATION_JSON})
     public Response getMsg() {
-
         String result = "Hello World";
-
         return Response.status(200).entity(result).build();
-
     }
 }
